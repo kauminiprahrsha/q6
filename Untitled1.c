9d4e695b95b90bb1c5805eac97c16232df7875ca
kauminiprahrsha <148688303+kauminiprahrsha@users.noreply.github.com>
2023-12-17 23:30:17 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,143 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int level;
+int bs;  //bs = basic salary
+char jn; //jn = job number
+float gross_salary;
+float salary;
+
+
+printf("Enter the job number of the employee:");
+scanf("%s",&jn);
+printf("Enter the basic paymennt of the employee:");
+scanf("%d",&bs);
+printf("Enter the level of employee: level 0");
+scanf("%d",&level);
+
+
+if (level==1)
+{
+    gross_salary= bs+bs*0.25+1000+500 ;
+
+     if (gross_salary<2000)
+     {
+         printf("salary is %.3f",gross_salary);
+     }
+
+         else if ((2000<=gross_salary) && (gross_salary<4000))
+         {
+             salary = gross_salary - gross_salary * 0.03 ;
+             printf("salary is %.3f",salary);
+         }
+
+             else if((4000<=gross_salary) && (gross_salary<6000))
+             {
+                salary = gross_salary - gross_salary * 0.05 ;
+                printf("salary is %.3f",salary);
+             }
+
+                else if (gross_salary>=2000)
+                {
+                    salary = gross_salary - gross_salary * 0.08 ;
+                    printf("salary is %.3f",salary);
+                }
+
+}
+
+else if (level==2)
+{
+    gross_salary= bs+bs*0.25+750+200 ;
+
+     if (gross_salary<2000)
+     {
+         printf("salary is %.3f",gross_salary);
+     }
+
+         else if (2000<=gross_salary && gross_salary<4000)
+         {
+             salary = gross_salary - gross_salary * 0.03 ;
+             printf("salary is %.3f",salary);
+         }
+
+             else if(4000<=gross_salary && gross_salary<6000)
+             {
+                salary = gross_salary - gross_salary * 0.05 ;
+                printf("salary is %.3f",salary);
+             }
+
+                else if (gross_salary>=2000)
+                {
+                    salary = gross_salary - gross_salary * 0.08 ;
+                    printf("salary is %.3f",salary);
+                }
+
+}
+else if (level==3)
+{
+    gross_salary= bs+bs*0.25+500+100 ;
+
+     if (gross_salary<2000)
+     {
+         printf("salary is %.3f",gross_salary);
+     }
+
+         else if (2000<=gross_salary && gross_salary<4000)
+         {
+             salary = gross_salary - gross_salary * 0.03 ;
+             printf("salary is %.3f",salary);
+         }
+
+             else if(4000<=gross_salary && gross_salary<6000)
+             {
+                salary = gross_salary - gross_salary * 0.05 ;
+                printf("salary is %.3f",salary);
+             }
+
+                else if (gross_salary>=2000)
+                {
+                    salary = gross_salary - gross_salary * 0.08 ;
+                    printf("salary is %.3f",salary);
+                }
+
+}
+else if (level==4)
+{
+    gross_salary= bs+bs*0.25+250 ;
+
+     if (gross_salary<2000)
+     {
+         printf("salary is %.3f",gross_salary);
+     }
+
+         else if (2000<=gross_salary && gross_salary<4000)
+         {
+             salary = gross_salary - gross_salary * 0.03 ;
+             printf("salary is %.3f",salary);
+         }
+
+             else if(4000<=gross_salary && gross_salary<6000)
+             {
+                salary = gross_salary - gross_salary * 0.05 ;
+                printf("salary is %.3f",salary);
+             }
+
+                else if (gross_salary>=2000)
+                {
+                    salary = gross_salary - gross_salary * 0.08 ;
+                    printf("salary is %.3f",salary);
+                }
+
+}
+ if (level!=1 && level!=2 && level!=3 && level!=4 )
+{
+    printf(" Please Enter a valid level number");
+}
+
+
+
+ return 0;
+}
